ec291b0dca9e46bdb3c97ed20878b9555e9c6f90
jwson <sonjungwoo9@gmail.com>
2023-08-01 12:43:34 +0900

    Initial commit

---
+++ app/src/main/java/com/project/tmp/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.project.tmp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val button = findViewById<Button>(R.id.LoginBtn)
+
+        button.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
